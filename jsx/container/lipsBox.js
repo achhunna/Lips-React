28cf1c4220e555fc09b2175a55a0257d0a6da59a
Achhunna M <achhunna@users.noreply.github.com>
2017-04-23 22:54:07 -0700

    Removing comments

--- jsx/container/lipsBox.js
+++ jsx/container/lipsBox.js
@@ -15,59 +15,7 @@ const mapStoreToProps = (store) => {
 };
 
 class LipsBox extends React.Component {
-    /*
-    constructor(props) {
-        super(props);
-        this.state = {
-            //ipsums: [],
-            count: 5,
-            selectedOption: "paragraphs",
-        };
-        this.updateCount = this.updateCount.bind(this);
-        this.updateOption = this.updateOption.bind(this);
-    }
-    componentDidMount() {
-        axios.get('./random.txt').then(response => {
-            let response_array = response.data.split("\n");
-            response_array = response_array.filter((x) => {return (x !== "");});
-            this.setState({ipsums: response_array});
-        });
-    }
-    parseText(content) {
-        if(content !== undefined) {
-            switch(this.state.selectedOption) {
-                case "paragraphs":
-                    return content;
-                case "words":
-                    return content.split(" ")[0];
-                case "sentences":
-                    return content.split(".")[0] +". ";
-            }
-        }
-    }
-    componentDidMount() {
-
-    }
-    updateCount(new_count) {
-        this.setState({
-            count: new_count
-        })
-    }
-    updateOption(new_option) {
-        this.setState({
-            selectedOption: new_option,
-        })
-    }*/
     render() {
-        /*
-        for (let i = 0; i < this.state.count; i++) {
-            let random_index = Math.floor(Math.random() * this.state.ipsums.length);
-            let random = this.state.ipsums[random_index];
-            let return_text = this.parseText(random);
-            output.push(<Section text={return_text} key={counter} selectedOption={this.state.selectedOption} />);
-            counter++;
-        }
-        */
         const maxCount = 100;
         let generatedText = "";
         let output = [];
